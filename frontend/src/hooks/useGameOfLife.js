0a4572302f34dc2507438a6976dd4dd63834d285
Alexey <xxxtrihardxxx@gmail.com>
2023-07-23 17:43:01 +0300

    update speed

--- frontend/src/hooks/useGameOfLife.js
+++ frontend/src/hooks/useGameOfLife.js
@@ -97,7 +97,7 @@ export const useLifecycle = (running, speed, fetchNextGeneration, isEmpty, reset
             }
             const interval = setInterval(() => {
                 fetchNextGeneration();
-            }, 500 / speed);
+            }, 1000 / speed);
             return () => clearInterval(interval);
         }
     }, [running, speed, fetchNextGeneration]);
