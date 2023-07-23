4ae775172b27b9844e4f3fcf8f99982b069b995f
Alexey <xxxtrihardxxx@gmail.com>
2023-07-23 19:58:12 +0300

    update css

--- frontend/src/components/ControlButton.jsx
+++ frontend/src/components/ControlButton.jsx
@@ -3,6 +3,6 @@ import '../styles/ControlButton.css'
 
 export const ControlButton = ({onClick, label}) => {
     return (
-        <button className="control-button" onClick={onClick}>{label} </button>
+        <button type={"button"} className="control-button" onClick={onClick}>{label} </button>
     );
 };
